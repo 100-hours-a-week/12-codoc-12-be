548756e239e65042f4dfafcf42c395471afe7c13
GONGRYONGWANG <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-01-19 20:18:16 +0900

    [docs] 이슈/PR 템플릿 생성

---
+++ src/main/java/_ganzi/codoc/CodocApplication.java
@@ -0,0 +1,13 @@
+package _ganzi.codoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/_ganzi/codoc/CodocApplicationTests.java
@@ -0,0 +1,13 @@
+package _ganzi.codoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

ed4ee76d43762f7b843fe4926c6066dcc93d757d
gnsdp <rladpgns99@gmail.com>
2026-01-20 10:42:01 +0900

    [feat] #2 공통 에러 클래스 및 핸들러 생성

---
+++ src/main/java/_ganzi/codoc/global/exception/ErrorCode.java
@@ -0,0 +1,12 @@
+package _ganzi.codoc.global.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCode {
+
+    HttpStatus status();
+
+    String code();
+
+    String message();
+}

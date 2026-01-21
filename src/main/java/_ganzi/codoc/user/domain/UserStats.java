9a4691b258380a0187d8fe77a2640e425d359982
gnsdp <rladpgns99@gmail.com>
2026-01-21 10:54:03 +0900

    [feat] #31 UserStats 엔티티 생성

---
+++ src/main/java/_ganzi/codoc/user/domain/UserStats.java
@@ -0,0 +1,35 @@
+package _ganzi.codoc.user.domain;
+
+import _ganzi.codoc.global.domain.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "user_stats")
+@Entity
+public class UserStats extends BaseTimeEntity {
+
+    @Id private Long id;
+
+    @Column(name = "xp", nullable = false)
+    private int xp;
+
+    @Column(name = "solved_cnt", nullable = false)
+    private int solvedCount;
+
+    @Column(name = "solving_cnt", nullable = false)
+    private int solvingCount;
+
+    @Column(name = "streak", nullable = false)
+    private int streak;
+
+    @Column(name = "deleted_at", nullable = false)
+    private Instant deletedAt;
+}

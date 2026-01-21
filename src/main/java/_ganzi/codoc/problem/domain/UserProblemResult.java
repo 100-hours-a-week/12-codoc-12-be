9e316f3c0287e4980c63b004aaeb70d3ce241d0c
gnsdp <rladpgns99@gmail.com>
2026-01-21 11:03:01 +0900

    [feat] #31 UserProblemResult 엔티티 생성

---
+++ src/main/java/_ganzi/codoc/problem/domain/UserProblemResult.java
@@ -0,0 +1,33 @@
+package _ganzi.codoc.problem.domain;
+
+import _ganzi.codoc.global.domain.BaseTimeEntity;
+import _ganzi.codoc.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+        name = "user_problem_result",
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "problem_id"})})
+@Entity
+public class UserProblemResult extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User userId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "problem_id", nullable = false)
+    private Problem problem;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false, length = 50)
+    private ProblemSolvingStatus status;
+}

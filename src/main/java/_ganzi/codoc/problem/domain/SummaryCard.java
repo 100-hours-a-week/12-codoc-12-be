78e20115548be52716aa9bbb55ea7e4e35ab3c7f
gnsdp <rladpgns99@gmail.com>
2026-01-21 11:03:40 +0900

    [feat] #31 SummaryCard 엔티티 생성

---
+++ src/main/java/_ganzi/codoc/problem/domain/SummaryCard.java
@@ -0,0 +1,36 @@
+package _ganzi.codoc.problem.domain;
+
+import _ganzi.codoc.global.domain.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "summary_card")
+@Entity
+public class SummaryCard extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "problem_id", nullable = false)
+    private Problem problem;
+
+    @Column(name = "choice", nullable = false, columnDefinition = "json")
+    private String choice;
+
+    @Column(name = "answer", nullable = false, columnDefinition = "json")
+    private String answer;
+}

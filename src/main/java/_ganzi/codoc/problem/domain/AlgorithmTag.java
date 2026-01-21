fbb81a661b001fc5f079123fd6b9d21cff902ff5
gnsdp <rladpgns99@gmail.com>
2026-01-21 10:56:52 +0900

    [feat] #31 AlgorithmTag 엔티티 생성

---
+++ src/main/java/_ganzi/codoc/problem/domain/AlgorithmTag.java
@@ -0,0 +1,26 @@
+package _ganzi.codoc.problem.domain;
+
+import _ganzi.codoc.global.domain.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "algorithm_tag")
+@Entity
+public class AlgorithmTag extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+}

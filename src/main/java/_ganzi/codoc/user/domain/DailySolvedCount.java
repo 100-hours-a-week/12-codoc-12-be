a10c5da90f9b991fec2d68c060cf49d0219deaac
gnsdp <rladpgns99@gmail.com>
2026-01-21 11:02:35 +0900

    [feat] #31 DailySolvedCount 엔티티 생성

---
+++ src/main/java/_ganzi/codoc/user/domain/DailySolvedCount.java
@@ -0,0 +1,31 @@
+package _ganzi.codoc.user.domain;
+
+import _ganzi.codoc.global.domain.BaseTimeEntity;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+        name = "daily_solved_count",
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "date"})})
+@Entity
+public class DailySolvedCount extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User userId;
+
+    @Column(name = "solved_count", nullable = false)
+    private int solvedCount;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+}

f77132f8d808e5f6095a44b7efc22cc5482aafcf
gnsdp <rladpgns99@gmail.com>
2026-01-21 10:55:45 +0900

    [feat] #31 UserQuest 엔티티 생성

---
+++ src/main/java/_ganzi/codoc/user/domain/UserQuest.java
@@ -0,0 +1,34 @@
+package _ganzi.codoc.user.domain;
+
+import _ganzi.codoc.global.domain.BaseTimeEntity;
+import jakarta.persistence.*;
+import java.time.Instant;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "user_quest")
+@Entity
+public class UserQuest extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User userId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "quest_id", nullable = false)
+    private Quest quest;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false, length = 50)
+    private QuestStatus status;
+
+    @Column(name = "expires_at", nullable = false)
+    private Instant expiresAt;
+}

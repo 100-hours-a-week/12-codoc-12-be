a9e2f44f1e421aa4d94e45b46f4ea89dec1febec
GONGRYONGWANG <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-01-22 09:00:31 +0900

    [feat] #44 유저 레포지토리 구현

--- src/main/java/_ganzi/codoc/user/domain/UserRepository.java
+++ src/main/java/_ganzi/codoc/user/domain/UserRepository.java
@@ -1,5 +1,17 @@
 package _ganzi.codoc.user.domain;
 
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    List<User> findAllByStatus(UserStatus status);
+
+    List<User> findAllByStatusAndLastAccessBefore(UserStatus status, Instant lastAccess);
+
+    Optional<User> findByIdAndStatus(Long id, UserStatus status);
+
+    boolean existsByNicknameAndIdNot(String nickname, Long id);
+}

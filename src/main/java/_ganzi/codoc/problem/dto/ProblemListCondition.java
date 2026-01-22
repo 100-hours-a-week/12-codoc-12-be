86a9b8ab8eafa1ec9231648df65c3876f3098235
gnsdp <rladpgns99@gmail.com>
2026-01-22 16:19:56 +0900

    [feat] #1 문제 목록 조회 Service 메서드 구현

---
+++ src/main/java/_ganzi/codoc/problem/dto/ProblemListCondition.java
@@ -0,0 +1,17 @@
+package _ganzi.codoc.problem.dto;
+
+import _ganzi.codoc.problem.domain.ProblemLevel;
+import _ganzi.codoc.problem.domain.ProblemSolvingStatus;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
+import java.util.List;
+import lombok.Builder;
+
+@Builder
+public record ProblemListCondition(
+        @Positive Long cursor,
+        @Min(1) @Max(50) int limit,
+        List<ProblemLevel> levels,
+        List<ProblemSolvingStatus> statuses,
+        boolean bookmarked) {}

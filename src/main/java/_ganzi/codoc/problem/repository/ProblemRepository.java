c4acf0f0ef97e8dd3928cf38e5a624d048f0d407
gnsdp <rladpgns99@gmail.com>
2026-01-23 02:31:47 +0900

    [feat] #12 문제 검색 Repository 메서드 구현

--- src/main/java/_ganzi/codoc/problem/repository/ProblemRepository.java
+++ src/main/java/_ganzi/codoc/problem/repository/ProblemRepository.java
@@ -41,4 +41,36 @@ public interface ProblemRepository extends JpaRepository<Problem, Long> {
             @Param("defaultStatus") ProblemSolvingStatus defaultStatus,
             @Param("bookmarked") boolean bookmarked,
             Pageable pageable);
+
+    @Query(
+            """
+            select new _ganzi.codoc.problem.dto.ProblemListItem(
+                p.id,
+                p.title,
+                p.level,
+                coalesce(upr.status, :defaultStatus),
+                (b.id is not null)
+            )
+            from Problem p
+            left join UserProblemResult upr on upr.problem = p and upr.user.id = :userId
+            left join Bookmark b on b.problem = p and b.user.id = :userId
+            where (:cursor is null or p.id > :cursor)
+              and p.level in :levels
+              and (
+                    upr.status in :statuses
+                    or (upr is null and :defaultStatus in :statuses)
+                  )
+              and (:bookmarked = false or b.id is not null)
+              and lower(p.title) like lower(concat('%', :query, '%'))
+            order by p.id asc
+            """)
+    List<ProblemListItem> searchProblemList(
+            @Param("userId") Long userId,
+            @Param("query") String query,
+            @Param("cursor") Long cursor,
+            @Param("levels") List<ProblemLevel> levels,
+            @Param("statuses") List<ProblemSolvingStatus> statuses,
+            @Param("defaultStatus") ProblemSolvingStatus defaultStatus,
+            @Param("bookmarked") boolean bookmarked,
+            Pageable pageable);
 }

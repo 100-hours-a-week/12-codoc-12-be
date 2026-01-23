9c58c9c0c32bb53bb7432d88fb1a2dff83e942e5
GONGRYONGWANG <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-01-23 17:15:07 +0900

    [fix] #46 avatar 테이블에 is_default 추가, is_default를 통해 기본 이미지 조회

--- src/main/java/_ganzi/codoc/user/domain/AvatarRepository.java
+++ src/main/java/_ganzi/codoc/user/domain/AvatarRepository.java
@@ -1,5 +1,9 @@
 package _ganzi.codoc.user.domain;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AvatarRepository extends JpaRepository<Avatar, Integer> {}
+public interface AvatarRepository extends JpaRepository<Avatar, Integer> {
+
+    Optional<Avatar> findByIsDefaultTrue();
+}

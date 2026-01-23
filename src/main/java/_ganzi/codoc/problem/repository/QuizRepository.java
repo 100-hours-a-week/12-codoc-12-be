355d88c6cd70278eee66b6624eac3031d8648980
gnsdp <rladpgns99@gmail.com>
2026-01-23 14:44:08 +0900

    [feat] #8 퀴즈 목록 조회 Repository 메서드 구현

---
+++ src/main/java/_ganzi/codoc/problem/repository/QuizRepository.java
@@ -0,0 +1,9 @@
+package _ganzi.codoc.problem.repository;
+
+import _ganzi.codoc.problem.domain.Quiz;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuizRepository extends JpaRepository<Quiz, Long> {
+    List<Quiz> findByProblemId(Long problemId);
+}

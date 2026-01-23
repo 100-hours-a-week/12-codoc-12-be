97f6bd5d273d2a3be95d178b839f0df38856f3c2
gnsdp <rladpgns99@gmail.com>
2026-01-23 20:11:31 +0900

    [feat] #8 ProblemLevel 숫자 변환 메서드 구현

--- src/main/java/_ganzi/codoc/problem/enums/ProblemLevel.java
+++ src/main/java/_ganzi/codoc/problem/enums/ProblemLevel.java
@@ -1,9 +1,19 @@
 package _ganzi.codoc.problem.enums;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum ProblemLevel {
-    ONE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    ;
+
+    private final int number;
+
+    public int toNumber() {
+        return number;
+    }
 }

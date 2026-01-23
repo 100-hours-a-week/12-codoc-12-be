83828364ef36019669cdc4dd6283c0c1f5dc53bd
GONGRYONGWANG <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-01-23 16:18:55 +0900

    [feat] #46 AVATAR_NOT_FOUND 에러 코드 추가 및 교체

--- src/main/java/_ganzi/codoc/user/exception/UserErrorCode.java
+++ src/main/java/_ganzi/codoc/user/exception/UserErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum UserErrorCode implements ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_NOT_FOUND", "사용자를 찾을 수 없습니다."),
-    DUPLICATE_NICKNAME(HttpStatus.CONFLICT, "DUPLICATE_NICKNAME", "이미 사용 중인 닉네임입니다.");
+    DUPLICATE_NICKNAME(HttpStatus.CONFLICT, "DUPLICATE_NICKNAME", "이미 사용 중인 닉네임입니다."),
+    AVATAR_NOT_FOUND(HttpStatus.NOT_FOUND, "AVATAR_NOT_FOUND", "아바타를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String code;

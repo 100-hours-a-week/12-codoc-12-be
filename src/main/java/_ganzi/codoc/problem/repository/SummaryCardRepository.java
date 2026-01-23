eb6050a167820c50cc79f87dccb1c53d74344c8f
gnsdp <rladpgns99@gmail.com>
2026-01-23 21:06:56 +0900

    [feat] #8 퀴즈 및 문제 요약 카드를 순서 기반으로 조회

--- src/main/java/_ganzi/codoc/problem/repository/SummaryCardRepository.java
+++ src/main/java/_ganzi/codoc/problem/repository/SummaryCardRepository.java
@@ -3,7 +3,18 @@ package _ganzi.codoc.problem.repository;
 import _ganzi.codoc.problem.domain.SummaryCard;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SummaryCardRepository extends JpaRepository<SummaryCard, Long> {
-    List<SummaryCard> findByProblemId(Long problemId);
+
+    @Query(
+            """
+            select summaryCard
+            from SummaryCard summaryCard
+            join fetch summaryCard.summaryCardTag summaryCardTag
+            where summaryCard.problem.id = :problemId
+            order by summaryCardTag.sequence asc
+            """)
+    List<SummaryCard> findByProblemIdOrderBySummaryCardTagSequenceAsc(@Param("problemId") Long problemId);
 }

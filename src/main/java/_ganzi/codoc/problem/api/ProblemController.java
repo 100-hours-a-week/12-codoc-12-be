e1efbbae6c1c71993218481eb55abf244fc93a34
gnsdp <rladpgns99@gmail.com>
2026-01-23 20:13:29 +0900

    [feat] #8 문제 상세 조회 Controller 메서드 구현

--- src/main/java/_ganzi/codoc/problem/api/ProblemController.java
+++ src/main/java/_ganzi/codoc/problem/api/ProblemController.java
@@ -5,11 +5,13 @@ import _ganzi.codoc.global.dto.ApiResponse;
 import _ganzi.codoc.global.dto.CursorPagingResponse;
 import _ganzi.codoc.problem.dto.ProblemListCondition;
 import _ganzi.codoc.problem.dto.ProblemListItem;
+import _ganzi.codoc.problem.dto.ProblemResponse;
 import _ganzi.codoc.problem.service.ProblemService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,6 +38,15 @@ public class ProblemController {
 
         CursorPagingResponse<ProblemListItem, Long> response =
                 problemService.searchProblems(userId, condition);
+
+        return ResponseEntity.ok(ApiResponse.success(response));
+    }
+
+    @GetMapping("/{problemId}")
+    public ResponseEntity<ApiResponse<ProblemResponse>> getProblemDetail(
+            @CurrentUserId Long userId, @PathVariable Long problemId) {
+
+        ProblemResponse response = problemService.getProblemDetail(userId, problemId);
 
         return ResponseEntity.ok(ApiResponse.success(response));
     }

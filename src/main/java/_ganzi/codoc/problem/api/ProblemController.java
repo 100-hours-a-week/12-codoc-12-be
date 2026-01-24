7cbf32eda98ac8a01254c7f7dc5d836ee1dacd77
gnsdp <rladpgns99@gmail.com>
2026-01-24 18:19:37 +0900

    [feat] #13 문제 북마크 등록 Controller 메서드 구현

--- src/main/java/_ganzi/codoc/problem/api/ProblemController.java
+++ src/main/java/_ganzi/codoc/problem/api/ProblemController.java
@@ -6,12 +6,14 @@ import _ganzi.codoc.global.dto.CursorPagingResponse;
 import _ganzi.codoc.problem.dto.ProblemListCondition;
 import _ganzi.codoc.problem.dto.ProblemListItem;
 import _ganzi.codoc.problem.dto.ProblemResponse;
+import _ganzi.codoc.problem.service.ProblemBookmarkService;
 import _ganzi.codoc.problem.service.ProblemService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProblemController {
 
+    private final ProblemBookmarkService problemBookmarkService;
     private final ProblemService problemService;
 
     @GetMapping
@@ -49,5 +52,14 @@ public class ProblemController {
         ProblemResponse response = problemService.getProblemDetail(userId, problemId);
 
         return ResponseEntity.ok(ApiResponse.success(response));
+    }
+
+    @PutMapping("/{problemId}/bookmark")
+    public ResponseEntity<Void> registerBookmark(
+            @CurrentUserId Long userId, @PathVariable Long problemId) {
+
+        problemBookmarkService.registerBookmark(userId, problemId);
+
+        return ResponseEntity.noContent().build();
     }
 }

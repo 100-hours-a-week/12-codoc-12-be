382f87394533a73eed6d214cc687e8c398a7559d
GONGRYONGWANG <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-01-25 03:48:05 +0900

    [feat] #80 아바타 목록 조회 API

---
+++ src/main/java/_ganzi/codoc/user/api/AvatarController.java
@@ -0,0 +1,23 @@
+package _ganzi.codoc.user.api;
+
+import _ganzi.codoc.global.dto.ApiResponse;
+import _ganzi.codoc.user.service.AvatarService;
+import _ganzi.codoc.user.service.dto.UserAvatarListResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/avatars")
+public class AvatarController {
+
+    private final AvatarService avatarService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<UserAvatarListResponse>> getAvatars() {
+        return ResponseEntity.ok(ApiResponse.success(avatarService.getAvatarList()));
+    }
+}

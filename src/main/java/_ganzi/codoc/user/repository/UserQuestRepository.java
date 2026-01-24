a0841de40be4a9a5ddfd86ea5003ca3a03cc80ab
GONGRYONGWANG <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-01-25 01:35:27 +0900

    [feat] #76 유저-퀘스트 레포지토리 구현

---
+++ src/main/java/_ganzi/codoc/user/repository/UserQuestRepository.java
@@ -0,0 +1,20 @@
+package _ganzi.codoc.user.repository;
+
+import _ganzi.codoc.user.domain.User;
+import _ganzi.codoc.user.domain.UserQuest;
+import _ganzi.codoc.user.enums.QuestStatus;
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserQuestRepository extends JpaRepository<UserQuest, Long> {
+
+    List<UserQuest> findAllByUser(User user);
+
+    Optional<UserQuest> findByIdAndUser(Long id, User user);
+
+    List<UserQuest> findAllByUserAndExpiresAtBefore(User user, Instant now);
+
+    List<UserQuest> findAllByUserAndStatus(User user, QuestStatus status);
+}

2ecec0b59c6c359305b7ab2d4a26ea0d54d35530
gnsdp <rladpgns99@gmail.com>
2026-01-24 18:27:03 +0900

    [feat] #13 문제 북마크 삭제 Repository 메서드 구현

--- src/main/java/_ganzi/codoc/problem/repository/BookmarkRepository.java
+++ src/main/java/_ganzi/codoc/problem/repository/BookmarkRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
     boolean existsByUserIdAndProblemId(Long userId, Long problemId);
+
+    void deleteByUserIdAndProblemId(Long userId, Long problemId);
 }

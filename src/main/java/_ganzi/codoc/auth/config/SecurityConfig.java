dc2fb491215deb1b10764870b2f39f823073ef3b
GONGRYONGWANG <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-01-25 23:19:22 +0900

    [feat] #19 jwt 인증 필터 추가

--- src/main/java/_ganzi/codoc/auth/config/SecurityConfig.java
+++ src/main/java/_ganzi/codoc/auth/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package _ganzi.codoc.auth.config;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
+import _ganzi.codoc.auth.jwt.JwtAuthenticationFilter;
 import _ganzi.codoc.user.enums.UserStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,16 +10,24 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
                 .cors(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(
                         authorize ->
                                 authorize

f09265c2e01f53a731f86c6a2f0c7231d943e110
GONGRYONGWANG <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-01-25 22:51:34 +0900

    [feat] #19 커스텀 UserDetails 구현 (AuthUser)

---
+++ src/main/java/_ganzi/codoc/auth/domain/AuthUser.java
@@ -0,0 +1,46 @@
+package _ganzi.codoc.auth.domain;
+
+import _ganzi.codoc.user.enums.UserStatus;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public record AuthUser(Long userId, UserStatus status) implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + status.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return String.valueOf(userId);
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}

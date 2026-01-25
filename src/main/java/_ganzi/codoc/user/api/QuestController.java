44a9b1c3f09869b647558f2077b3c7f31670e67e
GONGRYONGWANG <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-01-26 02:40:05 +0900

    [refactor] #18 Replace `X-USER-ID` with `@AuthenticationPrincipal`

--- src/main/java/_ganzi/codoc/user/api/QuestController.java
+++ src/main/java/_ganzi/codoc/user/api/QuestController.java
@@ -1,15 +1,16 @@
 package _ganzi.codoc.user.api;
 
+import _ganzi.codoc.auth.domain.AuthUser;
 import _ganzi.codoc.global.dto.ApiResponse;
 import _ganzi.codoc.user.service.QuestService;
 import _ganzi.codoc.user.service.dto.QuestRewardResponse;
 import _ganzi.codoc.user.service.dto.UserQuestListResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,13 +23,14 @@ public class QuestController {
 
     @GetMapping
     public ResponseEntity<ApiResponse<UserQuestListResponse>> getUserQuests(
-            @RequestHeader("X-USER-ID") Long userId) {
-        return ResponseEntity.ok(ApiResponse.success(questService.getUserQuests(userId)));
+            @AuthenticationPrincipal AuthUser authUser) {
+        return ResponseEntity.ok(ApiResponse.success(questService.getUserQuests(authUser.userId())));
     }
 
     @PostMapping("/{userQuestId}")
     public ResponseEntity<ApiResponse<QuestRewardResponse>> claimQuestReward(
-            @RequestHeader("X-USER-ID") Long userId, @PathVariable Long userQuestId) {
-        return ResponseEntity.ok(ApiResponse.success(questService.claimReward(userId, userQuestId)));
+            @AuthenticationPrincipal AuthUser authUser, @PathVariable Long userQuestId) {
+        return ResponseEntity.ok(
+                ApiResponse.success(questService.claimReward(authUser.userId(), userQuestId)));
     }
 }

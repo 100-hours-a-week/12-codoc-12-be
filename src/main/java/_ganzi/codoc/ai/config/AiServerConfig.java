b8a3f73399f4d3cf54270e69ea71e1d0c98d6a3e
gnsdp <rladpgns99@gmail.com>
2026-01-25 15:04:34 +0900

    [chore] #37 FastAPI 서버 설정 클래스 추가

---
+++ src/main/java/_ganzi/codoc/ai/config/AiServerConfig.java
@@ -0,0 +1,20 @@
+package _ganzi.codoc.ai.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@RequiredArgsConstructor
+@EnableConfigurationProperties(AiServerProperties.class)
+@Configuration
+public class AiServerConfig {
+
+    private final AiServerProperties aiServerProperties;
+
+    @Bean
+    public WebClient.Builder aiServerWebClientBuilder() {
+        return WebClient.builder().baseUrl(aiServerProperties.baseUrl());
+    }
+}

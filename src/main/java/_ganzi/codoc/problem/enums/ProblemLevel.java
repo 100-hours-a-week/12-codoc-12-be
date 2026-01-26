bc67ccef2a02c94e5d03f5e79e4a9e4aa30cf885
gnsdp <rladpgns99@gmail.com>
2026-01-26 20:13:53 +0900

    [remove] #113 ProblemLevel enum 필드 삭제
    - UI 메시지는 클라이언트에서 처리

--- src/main/java/_ganzi/codoc/problem/enums/ProblemLevel.java
+++ src/main/java/_ganzi/codoc/problem/enums/ProblemLevel.java
@@ -1,19 +1,10 @@
 package _ganzi.codoc.problem.enums;
 
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
 public enum ProblemLevel {
-    ONE(1),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
     ;
-
-    private final int number;
-
-    public int toNumber() {
-        return number;
-    }
 }

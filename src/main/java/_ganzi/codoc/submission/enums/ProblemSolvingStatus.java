cf53113393a0446e62aeb8d884c70b74f6e5f32e
gnsdp <rladpgns99@gmail.com>
2026-01-26 20:20:09 +0900

    [refactor] #113 ProblemSolvingStatus enum 값 변경

--- src/main/java/_ganzi/codoc/submission/enums/ProblemSolvingStatus.java
+++ src/main/java/_ganzi/codoc/submission/enums/ProblemSolvingStatus.java
@@ -6,19 +6,19 @@ import lombok.RequiredArgsConstructor;
 public enum ProblemSolvingStatus {
     NOT_ATTEMPTED,
     IN_PROGRESS,
-    SUMMARY_PASSED,
+    SUMMARY_CARD_PASSED,
     SOLVED,
     ;
 
     public ProblemSolvingStatus nextStatusForSummaryCard(boolean allCorrect) {
-        if (this == SUMMARY_PASSED || this == SOLVED) {
+        if (this == SUMMARY_CARD_PASSED || this == SOLVED) {
             return this;
         }
 
-        return allCorrect ? SUMMARY_PASSED : IN_PROGRESS;
+        return allCorrect ? SUMMARY_CARD_PASSED : IN_PROGRESS;
     }
 
     public boolean summaryCardPassed() {
-        return this == SUMMARY_PASSED || this == SOLVED;
+        return this == SUMMARY_CARD_PASSED || this == SOLVED;
     }
 }

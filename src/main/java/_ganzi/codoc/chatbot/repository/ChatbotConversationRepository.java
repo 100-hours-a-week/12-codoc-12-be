0f6884dd773d6a8e44e855f5c3ec0a156f1c933b
gnsdp <rladpgns99@gmail.com>
2026-01-27 16:20:01 +0900

    [feat] #16 챗봇 메시지 스트림 응답 Service 메서드 구현

--- src/main/java/_ganzi/codoc/chatbot/repository/ChatbotConversationRepository.java
+++ src/main/java/_ganzi/codoc/chatbot/repository/ChatbotConversationRepository.java
@@ -1,6 +1,17 @@
 package _ganzi.codoc.chatbot.repository;
 
 import _ganzi.codoc.chatbot.domain.ChatbotConversation;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface ChatbotConversationRepository extends JpaRepository<ChatbotConversation, Long> {}
+public interface ChatbotConversationRepository extends JpaRepository<ChatbotConversation, Long> {
+
+    @Query(
+            """
+            select conversation from ChatbotConversation conversation join fetch conversation.attempt
+             where conversation.id = :conversationId
+            """)
+    Optional<ChatbotConversation> findByIdWithAttempt(@Param("conversationId") Long conversationId);
+}

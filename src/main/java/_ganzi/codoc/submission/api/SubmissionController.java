bca15eb3614239fa8c98a4dfd0ded539642cd21a
gnsdp <rladpgns99@gmail.com>
2026-01-27 09:03:22 +0900

    [refactor] #113 제출 API URI 및 이름 변경

--- src/main/java/_ganzi/codoc/submission/api/SubmissionController.java
+++ src/main/java/_ganzi/codoc/submission/api/SubmissionController.java
@@ -2,10 +2,8 @@ package _ganzi.codoc.submission.api;
 
 import _ganzi.codoc.global.annotation.CurrentUserId;
 import _ganzi.codoc.global.dto.ApiResponse;
-import _ganzi.codoc.submission.dto.QuizGradingRequest;
-import _ganzi.codoc.submission.dto.QuizGradingResponse;
-import _ganzi.codoc.submission.dto.SummaryCardGradingRequest;
-import _ganzi.codoc.submission.dto.SummaryCardGradingResponse;
+import _ganzi.codoc.submission.dto.*;
+import _ganzi.codoc.submission.service.ProblemSubmissionService;
 import _ganzi.codoc.submission.service.QuizSubmissionService;
 import _ganzi.codoc.submission.service.SummaryCardSubmissionService;
 import jakarta.validation.Valid;
@@ -18,32 +16,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/submission")
+@RequestMapping("/api")
 @RestController
 public class SubmissionController {
 
     private final SummaryCardSubmissionService summaryCardSubmissionService;
     private final QuizSubmissionService quizSubmissionService;
+    private final ProblemSubmissionService problemSubmissionService;
 
-    @PostMapping("/problems/{problemId}/summary-cards")
+    @PostMapping("/summary-cards/submissions")
     public ResponseEntity<ApiResponse<SummaryCardGradingResponse>> gradeSummaryCards(
-            @CurrentUserId Long userId,
-            @PathVariable Long problemId,
-            @Valid @RequestBody SummaryCardGradingRequest request) {
+            @CurrentUserId Long userId, @Valid @RequestBody SummaryCardGradingRequest request) {
 
         SummaryCardGradingResponse response =
-                summaryCardSubmissionService.gradeSummaryCards(userId, problemId, request);
+                summaryCardSubmissionService.gradeSummaryCards(userId, request);
 
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
-    @PostMapping("/quizzes/{quizId}")
+    @PostMapping("/quizzes/{quizId}/submissions")
     public ResponseEntity<ApiResponse<QuizGradingResponse>> gradeQuiz(
             @CurrentUserId Long userId,
             @PathVariable Long quizId,
             @Valid @RequestBody QuizGradingRequest request) {
 
         QuizGradingResponse response = quizSubmissionService.gradeQuiz(userId, quizId, request);
+
+        return ResponseEntity.ok(ApiResponse.success(response));
+    }
+
+    @PostMapping("/problems/{problemId}/submissions")
+    public ResponseEntity<ApiResponse<ProblemSubmissionResponse>> submissionProblem(
+            @CurrentUserId Long userId, @PathVariable Long problemId) {
+
+        ProblemSubmissionResponse response =
+                problemSubmissionService.submissionProblem(userId, problemId);
 
         return ResponseEntity.ok(ApiResponse.success(response));
     }

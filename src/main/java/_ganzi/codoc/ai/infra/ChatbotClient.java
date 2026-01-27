b26dba1290bf8bc649078f54cf72212d610d7936
gnsdp <rladpgns99@gmail.com>
2026-01-27 15:13:25 +0900

    [feat] #16 챗봇 메시지 스트림 응답 클라이언트 메서드 구현

--- src/main/java/_ganzi/codoc/ai/infra/ChatbotClient.java
+++ src/main/java/_ganzi/codoc/ai/infra/ChatbotClient.java
@@ -5,14 +5,19 @@ import _ganzi.codoc.ai.dto.AiServerApiResponse;
 import _ganzi.codoc.ai.dto.AiServerChatbotSendRequest;
 import _ganzi.codoc.ai.dto.AiServerChatbotSendResponse;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
 @Component
 public class ChatbotClient {
 
     private static final String API_PATH_PREFIX_V1 = "/api/v1/chatbot";
     private static final String API_PATH_SEND_MESSAGE = API_PATH_PREFIX_V1;
+    private static final String API_PATH_STREAM_MESSAGE =
+            API_PATH_PREFIX_V1 + "/{conversationId}/stream";
 
     private final WebClient webClient;
     private final AiServerProperties aiServerProperties;
@@ -33,5 +38,14 @@ public class ChatbotClient {
                         new ParameterizedTypeReference<AiServerApiResponse<AiServerChatbotSendResponse>>() {})
                 .timeout(aiServerProperties.baseTimeout())
                 .block();
+    }
+
+    public Flux<ServerSentEvent<String>> streamMessage(Long conversationId) {
+        WebClient.RequestHeadersSpec<?> uri =
+                webClient.get().uri(API_PATH_STREAM_MESSAGE, conversationId);
+
+        return uri.accept(MediaType.TEXT_EVENT_STREAM)
+                .retrieve()
+                .bodyToFlux(new ParameterizedTypeReference<>() {});
     }
 }

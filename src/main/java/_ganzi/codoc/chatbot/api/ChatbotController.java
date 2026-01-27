a647799e1b881f85da93db66590644d9ac469402
gnsdp <rladpgns99@gmail.com>
2026-01-27 16:20:06 +0900

    [feat] #16 챗봇 메시지 스트림 응답 Controller 메서드 구현

--- src/main/java/_ganzi/codoc/chatbot/api/ChatbotController.java
+++ src/main/java/_ganzi/codoc/chatbot/api/ChatbotController.java
@@ -7,12 +7,12 @@ import _ganzi.codoc.chatbot.service.ChatbotService;
 import _ganzi.codoc.global.dto.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/chatbot")
@@ -29,5 +29,12 @@ public class ChatbotController {
         ChatbotMessageSendResponse response = chatbotService.sendMessage(authUser.userId(), request);
 
         return ResponseEntity.ok(ApiResponse.success(response));
+    }
+
+    @GetMapping(
+            value = "/messages/{conversationId}/stream",
+            produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ServerSentEvent<String>> streamMessage(@PathVariable Long conversationId) {
+        return chatbotService.streamMessage(conversationId);
     }
 }

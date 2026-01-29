7cd8329b4ff5ecc468500d1bbbda4e83f711dc7b
gnsdp <rladpgns99@gmail.com>
2026-01-29 15:36:21 +0900

    [feat] 문제 엔티티의 태그 컬럼을 List 타입으로 변경

--- src/main/java/_ganzi/codoc/problem/domain/Problem.java
+++ src/main/java/_ganzi/codoc/problem/domain/Problem.java
@@ -1,8 +1,10 @@
 package _ganzi.codoc.problem.domain;
 
+import _ganzi.codoc.global.converter.StringListConverter;
 import _ganzi.codoc.global.domain.BaseTimeEntity;
 import _ganzi.codoc.problem.enums.ProblemDifficulty;
 import jakarta.persistence.*;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,4 +28,8 @@ public class Problem extends BaseTimeEntity {
     @Enumerated(EnumType.STRING)
     @Column(name = "difficulty", nullable = false, length = 20)
     private ProblemDifficulty difficulty;
+
+    @Convert(converter = StringListConverter.class)
+    @Column(name = "tags", nullable = false, columnDefinition = "json")
+    private List<String> tags;
 }

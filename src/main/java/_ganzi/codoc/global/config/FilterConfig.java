0b75398b77ad5862863d7aa24f8dc0bd8fbcb7fc
jehyeok <67752010+jehyuck@users.noreply.github.com>
2026-02-10 09:01:25 +0900

    Revert "[fix] loadtest MDC 필터 순서 고정"
    This reverts commit 60b8e44c9ad634a6761c8daf923c975e4cbdb7cd.

--- src/main/java/_ganzi/codoc/global/config/FilterConfig.java
+++ src/main/java/_ganzi/codoc/global/config/FilterConfig.java
@@ -9,28 +9,15 @@ import org.springframework.context.annotation.Configuration;
 public class FilterConfig {
 
     @Bean
-    public LoadtestMdcFilter loadtestMdcFilter() {
-        return new LoadtestMdcFilter();
-    }
-
-    @Bean
     public LoggingContextFilter loggingContextFilter() {
         return new LoggingContextFilter();
-    }
-
-    @Bean
-    public FilterRegistrationBean<LoadtestMdcFilter> loadtestMdcFilterRegistration(
-            LoadtestMdcFilter filter) {
-        FilterRegistrationBean<LoadtestMdcFilter> bean = new FilterRegistrationBean<>(filter);
-        bean.setOrder(-200);
-        return bean;
     }
 
     @Bean
     public FilterRegistrationBean<LoggingContextFilter> loggingContextFilterRegistration(
             LoggingContextFilter filter) {
         FilterRegistrationBean<LoggingContextFilter> bean = new FilterRegistrationBean<>(filter);
-        bean.setOrder(200);
+        bean.setOrder(Integer.MIN_VALUE);
         return bean;
     }
 }

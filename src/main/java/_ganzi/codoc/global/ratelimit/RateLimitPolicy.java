3fdb0951006de27f1d5431ae295988295f2ff8fe
gnsdp <rladpgns99@gmail.com>
2026-02-11 17:35:49 +0900

    [feat] Rate Limit 정책 클래스 생성

---
+++ src/main/java/_ganzi/codoc/global/ratelimit/RateLimitPolicy.java
@@ -0,0 +1,18 @@
+package _ganzi.codoc.global.ratelimit;
+
+import java.time.Duration;
+import lombok.Builder;
+
+@Builder
+public record RateLimitPolicy(
+        Boolean enabled, Integer limit, Duration period, Integer consumeToken) {
+
+    public static RateLimitPolicy from(RateLimitProperties.Policy policy) {
+        return RateLimitPolicy.builder()
+                .enabled(policy.enabled())
+                .limit(policy.limit())
+                .period(policy.period())
+                .consumeToken(policy.consumeToken())
+                .build();
+    }
+}

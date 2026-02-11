cedcb6080d510e46d7a4d1a6dc0535f20a0db9ba
gnsdp <rladpgns99@gmail.com>
2026-02-11 16:03:39 +0900

    [feat] Rate Limit 초과 예외 클래스 생성

--- src/main/java/_ganzi/codoc/global/exception/GlobalErrorCode.java
+++ src/main/java/_ganzi/codoc/global/exception/GlobalErrorCode.java
@@ -12,6 +12,7 @@ public enum GlobalErrorCode implements ErrorCode {
     FORBIDDEN(HttpStatus.FORBIDDEN, "FORBIDDEN", "접근 권한이 없습니다."),
     RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "RESOURCE_NOT_FOUND", "요청한 리소스를 찾을 수 없습니다."),
     METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "METHOD_NOT_ALLOWED", "허용되지 않은 HTTP 메서드입니다."),
+    RATE_LIMIT_EXCEEDED(HttpStatus.TOO_MANY_REQUESTS, "RATE_LIMIT_EXCEEDED", "제한 요청 횟수를 초과했습니다."),
     CONFLICT(HttpStatus.CONFLICT, "CONFLICT", "요청이 충돌했습니다."),
     INTERNAL_SERVER_ERROR(
             HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "서버 내부 오류가 발생했습니다.");

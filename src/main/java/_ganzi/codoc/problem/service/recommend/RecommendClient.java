4bb7125db48e0e1cb8887ea9d552eb17e970bd57
GONGRYONGWANG <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-02-11 15:32:03 +0900

    [fix] AI 추천 문제 FastApi 호출 prefix 변경

--- src/main/java/_ganzi/codoc/problem/service/recommend/RecommendClient.java
+++ src/main/java/_ganzi/codoc/problem/service/recommend/RecommendClient.java
@@ -3,21 +3,24 @@ package _ganzi.codoc.problem.service.recommend;
 import _ganzi.codoc.analysis.config.AnalysisServerProperties;
 import _ganzi.codoc.problem.service.recommend.dto.RecommendRequest;
 import _ganzi.codoc.problem.service.recommend.dto.RecommendResponse;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
-@RequiredArgsConstructor
 public class RecommendClient {
 
     private static final String API_PATH_RECOMMEND = "/api/v2/recommend";
 
-    @Qualifier("analysisServerWebClientBuilder")
     private final WebClient.Builder analysisServerWebClientBuilder;
-
     private final AnalysisServerProperties analysisServerProperties;
+
+    public RecommendClient(
+            @Qualifier("analysisServerWebClientBuilder") WebClient.Builder analysisServerWebClientBuilder,
+            AnalysisServerProperties analysisServerProperties) {
+        this.analysisServerWebClientBuilder = analysisServerWebClientBuilder;
+        this.analysisServerProperties = analysisServerProperties;
+    }
 
     public RecommendResponse requestRecommendations(RecommendRequest request) {
         return analysisServerWebClientBuilder

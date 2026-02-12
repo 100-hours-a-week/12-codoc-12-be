432ac665e103a8640385bda6f63e498f6881d053
gnsdp <rladpgns99@gmail.com>
2026-02-12 13:31:55 +0900

    [feat] Rate Limit 응답 헤더 추가

--- src/main/java/_ganzi/codoc/global/ratelimit/RateLimitService.java
+++ src/main/java/_ganzi/codoc/global/ratelimit/RateLimitService.java
@@ -1,8 +1,8 @@
 package _ganzi.codoc.global.ratelimit;
 
-import _ganzi.codoc.global.exception.RateLimitExceededException;
 import io.github.bucket4j.Bucket;
 import io.github.bucket4j.ConsumptionProbe;
+import io.github.bucket4j.EstimationProbe;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,14 +14,16 @@ public class RateLimitService {
 
     private final RateLimitStore rateLimitStore;
 
-    public void tryConsume(String key, RateLimitPolicy policy) {
-
+    public RateLimitResult tryConsume(String key, RateLimitPolicy policy) {
         Bucket bucket = rateLimitStore.getBucket(key, policy);
         ConsumptionProbe probe = bucket.tryConsumeAndReturnRemaining(policy.consumeToken());
+        EstimationProbe resetProbe = bucket.estimateAbilityToConsume(policy.limit());
 
         if (!probe.isConsumed()) {
             log.warn("Rate Limit Exceeded - key={}", key);
-            throw new RateLimitExceededException();
         }
+
+        return new RateLimitResult(
+                probe, resetProbe.getNanosToWaitForRefill(), probe.getNanosToWaitForRefill());
     }
 }

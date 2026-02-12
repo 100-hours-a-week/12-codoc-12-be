72ddf6d9180eb69390fa5f478cd4601d8d722dd7
gnsdp <rladpgns99@gmail.com>
2026-02-12 17:14:36 +0900

    [feat] AI 챗봇 API 변경 사항 반영

--- src/main/java/_ganzi/codoc/ai/infra/ChatbotClient.java
+++ src/main/java/_ganzi/codoc/ai/infra/ChatbotClient.java
@@ -1,23 +1,18 @@
 package _ganzi.codoc.ai.infra;
 
-import _ganzi.codoc.ai.dto.AiServerApiResponse;
 import _ganzi.codoc.ai.dto.AiServerChatbotSendRequest;
-import _ganzi.codoc.ai.dto.AiServerChatbotSendResponse;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @Component
 public class ChatbotClient {
 
-    private static final String API_PATH_PREFIX_V1 = "/api/v1/chatbot";
-    private static final String API_PATH_SEND_MESSAGE = API_PATH_PREFIX_V1;
-    private static final String API_PATH_STREAM_MESSAGE =
-            API_PATH_PREFIX_V1 + "/{conversationId}/stream";
+    private static final String API_PATH_PREFIX = "/api/v2/chatbot";
+    private static final String API_PATH_STREAM_MESSAGE = API_PATH_PREFIX;
 
     private final WebClient webClient;
 
@@ -25,21 +20,12 @@ public class ChatbotClient {
         this.webClient = builder.build();
     }
 
-    public Mono<AiServerApiResponse<AiServerChatbotSendResponse>> sendMessage(
-            AiServerChatbotSendRequest request) {
+    public Flux<ServerSentEvent<String>> streamMessage(AiServerChatbotSendRequest request) {
         return webClient
                 .post()
-                .uri(API_PATH_SEND_MESSAGE)
+                .uri(API_PATH_STREAM_MESSAGE)
                 .bodyValue(request)
-                .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<>() {});
-    }
-
-    public Flux<ServerSentEvent<String>> streamMessage(Long conversationId) {
-        WebClient.RequestHeadersSpec<?> uri =
-                webClient.get().uri(API_PATH_STREAM_MESSAGE, conversationId);
-
-        return uri.accept(MediaType.TEXT_EVENT_STREAM)
+                .accept(MediaType.TEXT_EVENT_STREAM)
                 .retrieve()
                 .bodyToFlux(new ParameterizedTypeReference<>() {});
     }

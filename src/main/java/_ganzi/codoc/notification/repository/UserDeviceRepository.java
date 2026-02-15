85fda9cbdbdde6df09de5ffb00f13763472c9b0d
gnsdp <rladpgns99@gmail.com>
2026-02-15 18:43:20 +0900

    [feat] 유저 디바이스 등록/해제 API 구현

---
+++ src/main/java/_ganzi/codoc/notification/repository/UserDeviceRepository.java
@@ -0,0 +1,10 @@
+package _ganzi.codoc.notification.repository;
+
+import _ganzi.codoc.notification.domain.UserDevice;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDeviceRepository extends JpaRepository<UserDevice, Long> {
+
+    Optional<UserDevice> findByUserId(Long userId);
+}

ca9cd03bf89c039e5e0e0bfa0108041bba8af396
GONGRYONGWANG <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-02-24 13:24:31 +0900

    [feat] 리더 보드 테이블 추가, 도메인 구조 생성

---
+++ src/main/java/_ganzi/codoc/leaderboard/domain/LeaderboardScore.java
@@ -0,0 +1,39 @@
+package _ganzi.codoc.leaderboard.domain;
+
+import _ganzi.codoc.global.domain.BaseTimeEntity;
+import _ganzi.codoc.user.domain.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "leaderboard_score")
+@Entity
+public class LeaderboardScore extends BaseTimeEntity {
+
+    @EmbeddedId private LeaderboardScoreId id;
+
+    @MapsId("userId")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "league_id", nullable = false)
+    private League league;
+
+    @Column(name = "group_id")
+    private Long groupId;
+
+    @Column(name = "weekly_xp", nullable = false)
+    private int weeklyXp;
+}

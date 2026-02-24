95e52bc60ecc4488501938c70e5ee8c4d749603b
GONGRYONGWANG <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-02-24 14:18:53 +0900

    [feat] 리더보드 점수 업데이트 구현 (문제 해결, 퀘스트 완료)

--- src/main/java/_ganzi/codoc/leaderboard/repository/LeaderboardGroupMemberRepository.java
+++ src/main/java/_ganzi/codoc/leaderboard/repository/LeaderboardGroupMemberRepository.java
@@ -2,7 +2,11 @@ package _ganzi.codoc.leaderboard.repository;
 
 import _ganzi.codoc.leaderboard.domain.LeaderboardGroupMember;
 import _ganzi.codoc.leaderboard.domain.LeaderboardGroupMemberId;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LeaderboardGroupMemberRepository
-        extends JpaRepository<LeaderboardGroupMember, LeaderboardGroupMemberId> {}
+        extends JpaRepository<LeaderboardGroupMember, LeaderboardGroupMemberId> {
+
+    Optional<LeaderboardGroupMember> findFirstBySeasonIdAndUserId(Integer seasonId, Long userId);
+}

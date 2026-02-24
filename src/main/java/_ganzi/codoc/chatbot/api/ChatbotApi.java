fae89ee52b040eddd029fcb6c4327f555e2e11c1
gnsdp <rladpgns99@gmail.com>
2026-02-24 11:45:45 +0900

    [feat] 챗봇 대화 기록 조회 Controller 로직 구현

--- src/main/java/_ganzi/codoc/chatbot/api/ChatbotApi.java
+++ src/main/java/_ganzi/codoc/chatbot/api/ChatbotApi.java
@@ -1,15 +1,20 @@
 package _ganzi.codoc.chatbot.api;
 
 import _ganzi.codoc.auth.domain.AuthUser;
+import _ganzi.codoc.chatbot.dto.ChatbotConversationListCondition;
+import _ganzi.codoc.chatbot.dto.ChatbotConversationListItem;
 import _ganzi.codoc.chatbot.dto.ChatbotMessageSendRequest;
 import _ganzi.codoc.chatbot.exception.ChatbotErrorCode;
 import _ganzi.codoc.global.api.docs.ErrorCodes;
+import _ganzi.codoc.global.dto.ApiResponse;
+import _ganzi.codoc.global.dto.CursorPagingResponse;
 import _ganzi.codoc.global.exception.GlobalErrorCode;
 import _ganzi.codoc.problem.exception.ProblemErrorCode;
 import _ganzi.codoc.user.exception.UserErrorCode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.ServerSentEvent;
 import reactor.core.publisher.Flux;
 
@@ -52,4 +57,30 @@ public interface ChatbotApi {
             problem = {ProblemErrorCode.PROBLEM_NOT_FOUND},
             user = {UserErrorCode.USER_NOT_FOUND})
     Flux<ServerSentEvent<String>> sendAndStream(AuthUser authUser, ChatbotMessageSendRequest request);
+
+    @Operation(summary = "Get chatbot conversation history of current session by problem")
+    @ApiResponses({
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                responseCode = "400",
+                description = "INVALID_INPUT"),
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                responseCode = "401",
+                description = "AUTH_REQUIRED, UNAUTHORIZED"),
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                responseCode = "403",
+                description = "FORBIDDEN"),
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                responseCode = "500",
+                description = "INTERNAL_SERVER_ERROR")
+    })
+    @ErrorCodes(
+            global = {
+                GlobalErrorCode.INVALID_INPUT,
+                GlobalErrorCode.AUTH_REQUIRED,
+                GlobalErrorCode.UNAUTHORIZED,
+                GlobalErrorCode.FORBIDDEN,
+                GlobalErrorCode.INTERNAL_SERVER_ERROR
+            })
+    ResponseEntity<ApiResponse<CursorPagingResponse<ChatbotConversationListItem, Long>>>
+            getConversationList(AuthUser authUser, ChatbotConversationListCondition condition);
 }

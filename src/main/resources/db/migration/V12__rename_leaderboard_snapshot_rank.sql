8e6388ef74f9040e42c228f05e9b2387acce96ec
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-02-25 17:36:26 +0900

    Revert "[fix] 리더보드 스냅샷 테이블의 컬럼 name이 mySQL의 예약어와 (rank) 충돌하는 문제 해결"
    This reverts commit cdf8be804075e680e1204dee79a87abe132e2a37.

--- src/main/resources/db/migration/V12__rename_leaderboard_snapshot_rank.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `leaderboard_snapshot`
-  CHANGE COLUMN `rank` `rank_no` int NOT NULL;
-
-ALTER TABLE `leaderboard_snapshot`
-  DROP INDEX `idx_leaderboard_snapshot_scope`,
-  ADD KEY `idx_leaderboard_snapshot_scope` (`scope_type`, `scope_id`, `snapshot_id`, `rank_no`);

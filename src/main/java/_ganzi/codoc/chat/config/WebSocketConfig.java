874478528770bb8aea4cb498711239edb1d992e7
gnsdp <rladpgns99@gmail.com>
2026-02-26 21:01:03 +0900

    [chore] WebSocket STOMP 기본 인프라 설정 추가

---
+++ src/main/java/_ganzi/codoc/chat/config/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package _ganzi.codoc.chat.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final String frontendBaseUrl;
+
+    public WebSocketConfig(@Value("${app.frontend.base-url}") String frontendBaseUrl) {
+        this.frontendBaseUrl = frontendBaseUrl;
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-chat").setAllowedOrigins(frontendBaseUrl).withSockJS();
+    }
+}

b853c3db2d17dfd0f10317575f746a7edce2fc80
GONGRYONGWANG <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-02-26 16:03:37 +0900

    [feat] 문제 세션 TTL 설정과 생성/재사용 로직 추가

---
+++ src/main/java/_ganzi/codoc/submission/config/ProblemSessionConfig.java
@@ -0,0 +1,8 @@
+package _ganzi.codoc.submission.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@EnableConfigurationProperties(ProblemSessionProperties.class)
+@Configuration
+public class ProblemSessionConfig {}

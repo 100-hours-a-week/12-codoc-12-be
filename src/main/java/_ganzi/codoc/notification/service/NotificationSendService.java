bb425b57d6780cbfd141f18ebd44888d11c727e0
gnsdp <rladpgns99@gmail.com>
2026-03-06 09:35:43 +0900

    [refactor] 알림 발급 로직과 푸시 알림 전송 로직을 분리

--- src/main/java/_ganzi/codoc/notification/service/NotificationSendService.java
+++ src/main/java/_ganzi/codoc/notification/service/NotificationSendService.java
@@ -1,13 +1,8 @@
 package _ganzi.codoc.notification.service;
 
 import _ganzi.codoc.notification.domain.Notification;
-import _ganzi.codoc.notification.domain.UserNotificationPreference;
 import _ganzi.codoc.notification.dto.NotificationMessageItem;
-import _ganzi.codoc.notification.enums.NotificationType;
-import _ganzi.codoc.notification.enums.PushNotificationSendResult;
 import _ganzi.codoc.notification.repository.NotificationRepository;
-import _ganzi.codoc.notification.repository.UserDeviceRepository;
-import _ganzi.codoc.notification.repository.UserNotificationPreferenceRepository;
 import _ganzi.codoc.user.domain.User;
 import _ganzi.codoc.user.exception.UserNotFoundException;
 import _ganzi.codoc.user.repository.UserRepository;
@@ -21,10 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class NotificationSendService {
 
     private final UserRepository userRepository;
-    private final UserDeviceRepository userDeviceRepository;
-    private final UserNotificationPreferenceRepository userNotificationPreferenceRepository;
     private final NotificationRepository notificationRepository;
-    private final PushNotificationSender pushNotificationSender;
 
     @Transactional
     public void send(Long userId, NotificationMessageItem messageItem) {
@@ -41,27 +33,5 @@ public class NotificationSendService {
                             messageItem.type().getLinkCode(),
                             messageItem.linkParams()));
         }
-
-        if (!isPreferenceEnabled(userId, messageItem.type())) {
-            return;
-        }
-
-        userDeviceRepository
-                .findByUserIdAndActiveTrue(userId)
-                .ifPresent(
-                        userDevice -> {
-                            PushNotificationSendResult sendResult =
-                                    pushNotificationSender.send(messageItem, userDevice.getPushToken());
-                            if (sendResult == PushNotificationSendResult.INVALID_TOKEN) {
-                                userDevice.deactivate();
-                            }
-                        });
-    }
-
-    private boolean isPreferenceEnabled(Long userId, NotificationType type) {
-        return userNotificationPreferenceRepository
-                .findByUserIdAndType(userId, type)
-                .map(UserNotificationPreference::isEnabled)
-                .orElse(true);
     }
 }

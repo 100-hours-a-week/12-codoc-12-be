52eac2b8e394f0c6a6ccde2481a564531aaa5470
gnsdp <rladpgns99@gmail.com>
2026-03-09 00:35:21 +0900

    [chore] Caffeine 캐시 설정 추가

---
+++ src/main/java/_ganzi/codoc/global/constants/CacheNames.java
@@ -0,0 +1,14 @@
+package _ganzi.codoc.global.constants;
+
+public final class CacheNames {
+
+    // Cache Manager Bean Names
+    public static final String CAFFEINE_CACHE_MANAGER = "caffeineCacheManager";
+
+    // Cache Names
+    public static final String PROBLEM_DETAIL = "problem";
+    public static final String PROBLEM_SUMMARY_CARDS = "summary-cards";
+    public static final String PROBLEM_QUIZZES = "quizzes";
+
+    private CacheNames() {}
+}

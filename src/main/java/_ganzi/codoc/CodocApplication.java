fc1f76ef89ff36fc69b60b3cbce5233c16a71395
gnsdp <rladpgns99@gmail.com>
2026-03-10 17:41:22 +0900

    [chore] Scheduling 설정 파일 분리

--- src/main/java/_ganzi/codoc/CodocApplication.java
+++ src/main/java/_ganzi/codoc/CodocApplication.java
@@ -2,10 +2,8 @@ package _ganzi.codoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class CodocApplication {
 
     public static void main(String[] args) {

c5a3b3e5390ae3524201182757108f10a4eec72d
gnsdp <rladpgns99@gmail.com>
2026-03-17 10:21:04 +0900

    [feat] S3 연동 및 presigned url 로직 추가

---
+++ src/main/java/_ganzi/codoc/global/s3/ObjectStorageService.java
@@ -0,0 +1,12 @@
+package _ganzi.codoc.global.s3;
+
+import java.time.Duration;
+
+public interface ObjectStorageService {
+
+    String issueUploadUrl(String bucket, String key, String contentType, Duration expiration);
+
+    String issueDownloadUrl(String bucket, String key, Duration expiration);
+
+    void assertObjectExists(String bucket, String key);
+}

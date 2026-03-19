0e5768bcc2103b685b521359cab96cb6dccf4698
GONGRYONGWANG <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-03-19 17:07:58 +0900

    [feat] 리그 로고 url 추가 - flyway migration

---
+++ src/main/resources/db/migration/V20260319_001__update_league_logo_urls.sql
@@ -0,0 +1,24 @@
+UPDATE league
+SET logo_url = 'https://images.codoc.cloud/league/league3.png',
+    updated_at = NOW(6)
+WHERE name = 'BRONZE';
+
+UPDATE league
+SET logo_url = 'https://images.codoc.cloud/league/league4.png',
+    updated_at = NOW(6)
+WHERE name = 'SILVER';
+
+UPDATE league
+SET logo_url = 'https://images.codoc.cloud/league/league5.png',
+    updated_at = NOW(6)
+WHERE name = 'GOLD';
+
+UPDATE league
+SET logo_url = 'https://images.codoc.cloud/league/league6.png',
+    updated_at = NOW(6)
+WHERE name = 'PLATINUM';
+
+UPDATE league
+SET logo_url = 'https://images.codoc.cloud/league/league7.png',
+    updated_at = NOW(6)
+WHERE name = 'DIAMOND';
